78c04f4968f58a9afee2eb14c69ebb735e524b9b
abirami <abipoongodi1211@gmail.com>
2025-08-31 21:17:57 +0530

    psycopg2 update

--- backend/dbconnection.py
+++ backend/dbconnection.py
@@ -3,43 +3,40 @@ import psycopg2
 from dotenv import load_dotenv
 load_dotenv()  # Load environment variables from .env filse
 import os    
+import psycopg2.extras
 
 def get_db_connection():
-    conn = None
     try: 
-        conn = psycopg2.connect(
+        with psycopg2.connect(
             host=os.getenv("postgres_host"),
             database=os.getenv("postgres_db"),
             user=os.getenv("postgres_user"),
             password=os.getenv("postgres_password"),
             port=os.getenv("postgres_port")
-        )
-        print("Database connection established")
-        return conn
+        ) as conn:
+            print("Database connection established")
+            return conn
     except Exception as e:
         print(f"Error connecting to the database: {e}")
         return None
-       
+    finally:
+        print("Connection closed",conn.closed)
 def get_user_details():
     con = get_db_connection()
-    cursor=None
     try:
         if con is not None:
-            cursor = con.cursor()
-            script='''Select * from users;'''
-            cursor.execute(script)
-            rows = cursor.fetchall()
-            print(rows)
-            return rows
+           with con.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cursor:
+                script='''Select * from users;'''
+                cursor.execute(script)
+                rows = cursor.fetchall()
+                return rows
         return []
     except Exception as e:
         print(f"Error fetching user details: {e}")
         return []
     finally:
-        if cursor is not None:
-            cursor.close()
-        if con is not None:
-            con.close()
-            print("Database connection closed")
+        con.close()
+        print(con.closed)  # Check if connection is closed
+        print(cursor.closed)
 
 get_user_details()            
